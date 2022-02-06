bae9839824273f42314a3fb23620fb60cfe11933
Junior Paula <slucivaldo509@gmail.com>
2022-02-06 18:55:07 -0300

    create userContext

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Register from './pages/auth/register';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContainer from './components/MainContainer';
+import { UserProvider } from './context/userContex';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <MainContainer>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </MainContainer>
-      <Footer />
+      <UserProvider>
+        <Header />
+        <MainContainer>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </MainContainer>
+        <Footer />
+      </UserProvider>
     </BrowserRouter>
   );
 }
