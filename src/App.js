c76d7c8a4133d9a1d50700f71eae81881300ef18
Junior Paula <slucivaldo509@gmail.com>
2022-02-06 16:30:39 -0300

    Configurate react router dom and routes from aplication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+
 function App() {
-  return <h1>Hello dev!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
